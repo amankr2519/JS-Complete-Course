348e8b320550436a88d8e94616f43c6dc4e55443
Aman Kumar <ak3230531@gmail.com>
2025-01-08 22:55:16 +0530

    variable_datatype

---
+++ 02_Var_Datatype/index.js
@@ -0,0 +1,23 @@
+// console.log("Hello Javascript.........")
+
+// variables
+// 1) var - It allows redeclaration and updation
+// 2) let - It doesn't allow redeclaration but it allows updation
+// 3) const - It doesn't allow redeclaration and updation
+
+var x = 10 
+
+var x = 20 
+
+console.log(x)
+console.log(typeof(x))
+
+
+let y = 10 
+y = 20 
+console.log(y)
+
+
+const pi = 3.14 // It can't be updated
+console.log(pi)
+console.log(typeof(pi))
