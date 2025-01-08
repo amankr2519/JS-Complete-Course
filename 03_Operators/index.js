2fe8166cf76d179cf1103cc97900e03abffe7904
Aman Kumar <ak3230531@gmail.com>
2025-01-08 23:52:00 +0530

    operators_templateLiterals

---
+++ 03_Operators/index.js
@@ -0,0 +1,63 @@
+// operators
+
+
+// Arithmetic operator
+
+let a = 20 
+
+let b = 10
+
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a/b)
+// console.log(a%b)
+
+
+// Assignment Operator 
+
+let x = 10 
+let sum = 100
+
+sum = sum + x // shortcut -> sum += x
+// console.log(sum)
+
+sum *= 2
+// console.log(sum)
+
+// Comparison Operator
+
+let x1 = 100
+let x2 = 200
+/*
+console.log(x1>=x2) //F
+console.log(x1<=x2) //T
+console.log(x1==x2) //F
+console.log(x1===x2) //F It check value and type
+console.log(x1!=x2)  //T
+console.log(x1!==x2)  //T 
+
+*/
+
+// logical Opeartor - && , ||  , !
+
+
+// Ternary operator
+
+// (condtion) ? (True): (False)
+
+let marks = 50 
+
+const result = (marks>40) ? ("Pass") : ("Backlog")
+
+console.log(result)
+
+
+// template literal 
+
+let Name = "Aman "
+let income = "50LPA"
+let pincode = 110032
+
+console.log(`Your name is ${Name} and income is ${income} and your pincode is ${pincode}`)
+
