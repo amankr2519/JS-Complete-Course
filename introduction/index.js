aebc78e256f5824eee59db21ef54027f8f3bdacc
Aman Kumar <ak3230531@gmail.com>
2025-01-08 22:41:07 +0530

    first commit

---
+++ introduction/index.js
@@ -0,0 +1,12 @@
+console.log("Hello World")
+
+console.log("Aman Kumar")
+
+console.log("Enrollment no - 123")
+
+console.log("Batch - AIML")
+
+console.log("College - Usar")
+
+console.log("University - GGSIPU")
+
