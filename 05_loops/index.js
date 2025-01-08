67316d4e969db60c5044d8f235149727318e4696
Aman Kumar <ak3230531@gmail.com>
2025-01-09 00:32:07 +0530

    loops

---
+++ 05_loops/index.js
@@ -0,0 +1,49 @@
+console.log("We are learning loops")
+
+// console.log("Aman")
+// console.log("Aman")
+// console.log("Aman")
+// console.log("Aman")
+// console.log("Aman")
+// console.log("Aman")
+// console.log("Aman")
+// console.log("Aman")
+// console.log("Aman")
+// console.log("Aman")
+// console.log("Aman")
+
+
+// for(initilization ; condition ; updation){
+//   code .......
+// }
+
+for(let i = 0 ; i<10 ; i++){
+  console.log("Aman" , i)
+}
+
+// Table printing 
+// 2 X 1 = 2 
+
+let n = 2
+
+for(let i = 1 ; i<=10 ; i++){
+  console.log(`${n} X  ${i} = ${n*i}`)
+}
+
+// while Loop
+
+// initilization 
+// condition
+// updation
+
+let i = 10
+
+while(i!=0){
+  console.log(i)
+  i--
+}
+
+do {
+  console.log("Do while")
+  i--
+} while (i>5);
