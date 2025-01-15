f750f00f09d1289649326752d8608d625b82e50e
Aman Kumar <ak3230531@gmail.com>
2025-01-16 00:01:46 +0530

    advanced loops

---
+++ 09_Advanced_loops/index.js
@@ -0,0 +1,13 @@
+const phone = {
+  brand: "Apple",
+  model : "Iphone16",
+  price : 750000,
+  ram : "4gb",
+  rom : "64gb",
+  camera: "12MP"
+}
+// for in 
+for(let key in phone){
+  console.log(key,phone[key])
+
+}
