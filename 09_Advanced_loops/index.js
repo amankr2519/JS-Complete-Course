0a6249d3cf02d175aba804b6e615f59715665f60
Aman Kumar <ak3230531@gmail.com>
2025-01-16 13:55:12 +0530

    advanced loops (foreach,forof)

--- 09_Advanced_loops/index.js
+++ 09_Advanced_loops/index.js
@@ -11,3 +11,16 @@ for(let key in phone){
   console.log(key,phone[key])
 
 }
+
+// for of 
+
+let arr = [1,2,3,4,5];
+
+// for (const element of arr) {
+//   console.log(element);
+// }
+
+// forEach
+
+arr.forEach((value,index,arr)=>console.log(value,index,arr));
+
